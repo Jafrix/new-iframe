a675cc6da3a2cec912ecff723cd22c3771945cf7
Andrey Veselov <lifeforglory@gmail.com>
2025-07-24 11:18:02 +0300

    webpack v1.1

--- scripts/index.js
+++ scripts/index.js
@@ -8,10 +8,18 @@ function changeItem() {
   if (!flag) {
     if (!div) {
       div = document.createElement("div");
-      div.innerHTML = "i am DOG";
+      div.style = "margin-left: 15px; padding: 10px 20px; border-radius: 8px;";
+     div.innerHTML = `<img
+          class="fox-icon"
+          src="./assets/975549_OGFB9T1.svg"
+          alt="fox-icon"
+          width="200"
+          height="200"
+        />`;
     }
 
     button.after(div);
+
   } else {
     if (div && div.parentNode) {
       div.parentNode.removeChild(div);
